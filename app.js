4e63fe90fe39fe60981c62d089fe51197ac0267a
cituk <programaciondyn3.mx@sandos.com>
2020-02-05 22:41:21 -0500

    Conectando el proyecto server con MongoDB usando Mongoose

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const { API_VERSION } = require("./config");
+
+
+//load routings
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//configure header http
+
+//rutas basicas
+
+module.exports = app;

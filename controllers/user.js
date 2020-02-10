031e0e5027061fc22ee7d85728029c6f4bb2c3dd
cituk <programaciondyn3.mx@sandos.com>
2020-02-09 21:07:18 -0500

    Endpoint para crear nuevos usuarios, en mongo finalizado

--- controllers/user.js
+++ controllers/user.js
@@ -5,21 +5,37 @@ const User = require("../models/user");
 function signUp(req, res) {
     const user = new User();
 
-    const { email, password, repeatPassword } = req.body;
+    const { name, lastname, email, password, repeatPassword } = req.body;
+    user.name = name;
+    user.lastname = lastname;
     user.email = email;
     user.role = "admin";
     user.active = false;
 
     if (!password || !repeatPassword) {
-        res.status(404).send({ message: "Las contraseñas son obligatorias" })
+        res.status(404).send({ ok: false, message: "Las contraseñas son obligatorias" })
     } else {
         if (password !== repeatPassword) {
-            res.status(404).send({ message: "Las contraseñas tienen que ser iguales" })
+            res.status(404).send({ ok: false, message: "Las contraseñas tienen que ser iguales" })
         } else {
             bcrypt.hash(password, null, null, function(err, hash) {
-
-                })
-                //res.status(200).send({ message: "Usuario creado" })
+                if (err) {
+                    res.status(500).send({ ok: false, message: "Error al encriptar la contraseña" })
+                } else {
+                    user.password = hash;
+                    user.save((err, userSaved) => {
+                        if (err) {
+                            res.status(500).send({ ok: false, message: "Error: el usuario ya existe" + err })
+                        } else {
+                            if (!userSaved) {
+                                res.status(404).send({ ok: false, message: "Error al crear el usuario" })
+                            } else {
+                                res.status(200).send({ ok: true, user: userSaved })
+                            }
+                        }
+                    });
+                }
+            })
         }
     }
 

a6d133dc3d8901face61d1f290a6b5d1ac9a43f2
cituk <programaciondyn3.mx@sandos.com>
2020-02-10 15:05:30 -0500

    Creando el servicio para la creación de los tokens

---
+++ services/jwt.js
@@ -0,0 +1,32 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+
+const SECRET_KEY = "LGKU23T7fgs7668JSJVrfK4YHDldjdfRR78NOPZCBRg";
+
+exports.createAccessToken = function(user) {
+    const payload = {
+        id: user._id,
+        name: user.name,
+        lastname: user.lastname,
+        email: user.email,
+        role: user.role,
+        createToken: moment().unix(),
+        exp: moment.add(3, "hours").unix()
+
+    };
+    return jwt.encode(payload, SECRET_KEY);
+};
+
+exports.createRefresToken = function(user) {
+    const payload = {
+        id: user._id,
+        exp: moment.add(30, "days").unix()
+
+    };
+    return jwt.encode(payload, SECRET_KEY);
+};
+
+exports.decodedToken = function(token) {
+
+    return jwt.decode(token, SECRET_KEY, true);
+};

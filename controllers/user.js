a246e99e6ed4375eeb6c97e7df8ac45ffda01f8a
cituk <programaciondyn3.mx@sandos.com>
2020-02-09 20:45:12 -0500

    Endpoint para crear nuevos usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,29 @@ const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user");
 
 function signUp(req, res) {
-    console.log('Endpoint de signup');
+    const user = new User();
+
+    const { email, password, repeatPassword } = req.body;
+    user.email = email;
+    user.role = "admin";
+    user.active = false;
+
+    if (!password || !repeatPassword) {
+        res.status(404).send({ message: "Las contraseñas son obligatorias" })
+    } else {
+        if (password !== repeatPassword) {
+            res.status(404).send({ message: "Las contraseñas tienen que ser iguales" })
+        } else {
+            bcrypt.hash(password, null, null, function(err, hash) {
+
+                })
+                //res.status(200).send({ message: "Usuario creado" })
+        }
+    }
+
+
+
+
 }
 
 module.exports = {

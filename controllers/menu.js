a355df601ab6e8d468686d22f6d6a63730eee8fb
cituk <programaciondyn3.mx@sandos.com>
2020-02-19 22:18:42 -0500

    Endpoint para crear nuevos menús

--- controllers/menu.js
+++ controllers/menu.js
@@ -3,8 +3,25 @@ const Menu = require("../models/menu"); //modelo
 
 
 function addMenu(req, res) {
-    const user = new Menu();
-    console.log('add menu');
+    const { title, url, order, active } = req.body;
+    const menu = new Menu();
+
+    menu.title = title;
+    menu.url = url;
+    menu.order = order;
+    menu.active = active;
+
+    menu.save((err, menuCreado) => {
+        if (err) {
+            res.status(500).send({ ok: false, message: "Error de servidor" })
+        } else {
+            if (!menuCreado) {
+                res.status(404).send({ ok: false, message: "Error al crear el menú" })
+            } else {
+                res.status(200).send({ ok: true, message: "Menú creado correctamente", menu: menuCreado })
+            }
+        }
+    })
 }
 
 

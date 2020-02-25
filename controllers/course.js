5e2fda5a4bb97c8dee44803f4797b5c0c24ecedb
cituk <programaciondyn3.mx@sandos.com>
2020-02-24 22:23:41 -0500

    Endpoint para crear cursos

--- controllers/course.js
+++ controllers/course.js
@@ -3,38 +3,21 @@ const Course = require("../models/course"); //modelo
 
 function addCourse(req, res) {
 
-    console.log('hola');
-    /* const user = new Course();
+    const body = req.body;
+    const course = new Course(body);
+    course.order= 1000;
 
-    const { name, lastname, email, role, password } = req.body;
-    user.name = name;
-    user.lastname = lastname;
-    user.email = email.toLowerCase();
-    user.role = role;
-    user.active = true;
-
-    if (!password) {
-        res.status(500).send({ ok: false, message: "La contraseña es obligatoria" });
-    } else {
-        bcrypt.hash(password, null, null, (err, hash) => {
-            if (err) {
-                res.status(500).send({ ok: false, message: "Error al encriptar la contraseña" });
+    course.save((err, courseCreado) => {
+        if (err) {
+            res.status(400).send({ ok: false, code:400, message: "El curso ya existe" })
+        } else {
+            if (!courseCreado) {
+                res.status(400).send({ ok: false, code:400, message: "Error, no se pudo crear el curso" })
             } else {
-                user.password = hash;
-                user.save((err, usuarioGuardado) => {
-                    if (err) {
-                        res.status(500).send({ ok: false, message: "El usuario ya existe" });
-                    } else {
-                        if (!usuarioGuardado) {
-                            res.status(500).send({ ok: false, message: "Error al crear el nuevo usuario." });
-                        } else {
-                            res.status(200).send({ ok: true, message: "Usuario creado.", usuario: usuarioGuardado });
-                        }
-                    }
-                })
+                res.status(200).send({ ok: true, code:200,message: "Curso creado correctamente", course: courseCreado })
             }
-        });
-    } */
+        }
+    })
 }
 
 module.exports = {
